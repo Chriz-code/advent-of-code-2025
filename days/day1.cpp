3f743cfbcc1f266dc2f88e5e944c9ac501394b36
Chris <78105414+Chriz-code@users.noreply.github.com>
2025-12-01 22:27:07 +0100

    Solved part1 by using stupid

--- days/day1.cpp
+++ days/day1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 #include <string>
 
 using namespace std;
@@ -11,32 +12,72 @@ private:
     const string inputFile = "C:\\GitHub\\advent-of-code-2025\\days\\day1_input.txt";
     ifstream inputStream;
 
-public:
-    Day1()
+    void solver(string line, int &dial, int &result)
     {
-        inputStream = ifstream(inputFile);
+        int turn = stoi(line.substr(1));
+        if (line[0] == 'L')
+        {
+            dial -= turn;
+        }
+        else
+        {
+            dial += turn;
+        }
+
+        while(dial >= 100) {
+            dial -= 100;
+        }
+
+        while (dial < 0) {
+            dial = 100 + dial;
+        }
+
+        cout << "The dial is rotated " << line << " to point at ";
+        if (dial == 0)
+        {
+            result += 1;
+            cout << "*" << dial << "*";
+        }
+        else
+        {
+            cout << dial;
+        }
+        cout << "\n";
     }
 
+public:
+    int part1Test()
+    {
+        stringstream ss("L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82");
+        string line;
+        int result = 0;
+        int dial = 50;
+        while (getline(ss, line, '\n'))
+        {
+            solver(line, dial, result);
+        }
+        return result;
+    }
+
+    // The right answer is 1139
     int part1()
     {
+        ifstream inputStream(inputFile);
+
         if (!inputStream.is_open())
         {
             cout << "Unable to open file";
             return 0;
         }
-        
+
+        int result = 0;
+        int dial = 50;
+
         string line;
         while (getline(inputStream, line))
         {
-            cout << line << '\n';
+            solver(line, dial, result);
         }
-        inputStream.close();
-
-        return 0;
-    }
-
-    ~Day1()
-    {
-        inputStream.close();
+        return result;
     }
 };
--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@ int main(int, char**){
     std::cout << "Hello, from advent-of-code-2025!\n";
 
     Day1 day1;
-    day1.part1();
+    int testResult = day1.part1Test();
+    //cout << "The result of day1Test is " << testResult << endl;
+    cout << "The result of day1 is " << day1.part1() << endl;
+
 }
+//16
+//461
+//698
