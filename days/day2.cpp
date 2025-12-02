f99f0fc812935c1e234a3f143f259fb553ab4979
Chris <78105414+Chriz-code@users.noreply.github.com>
2025-12-02 21:17:07 +0100

    solved day 2 part 1

--- days/day2.cpp
+++ days/day2.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-class Day2 {
+struct Range
+{
+    long start;
+    long end;
+};
+
+class Day2
+{
+private:
     const string inputFile = "inputs/day2.txt";
     ifstream *stream()
     {
@@ -16,5 +25,54 @@ class Day2 {
             return nullptr;
         }
         return inputStream;
+    }
+
+    string readToString(ifstream *stream)
+    {
+        string line;
+        string result;
+        while (getline(*stream, line))
+        {
+            result += line;
+        }
+        return result;
+    }
+
+    bool isPalindrome(string value)
+    {
+        return value.length() % 2 == 0 && value.substr(0, value.length() / 2) == value.substr(value.length() / 2);
+    }
+
+    Range parseRange(string line)
+    {
+        int splitIdx = line.find('-');
+        string start = line.substr(0, splitIdx);
+        string end = line.substr(splitIdx + 1);
+        long startNum = stol(start);
+        long endNum = stol(end);
+        cout << startNum << ":" << splitIdx << ":" << endNum << endl;
+        return { startNum, endNum };
+    }
+
+public:
+    Day2 *part1()
+    {
+        stringstream input(readToString(stream()));
+        string line;
+        long sumOfPalindromes = 0L;
+        while (getline(input, line, ','))
+        {
+            Range range = parseRange(line);
+
+            for (long value = range.start; value <= range.end; value++)
+            {
+                if (isPalindrome(to_string(value)))
+                {
+                    sumOfPalindromes += value;
+                }
+            }
+        }
+        cout << "Here lies " << to_string(sumOfPalindromes) << endl;
+        return this;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "days/day1.cpp"
+#include "days/day2.cpp"
 
 int main(int, char**){
     std::cout << "Hello, from advent-of-code-2025!\n";
 
-    Day1 day1;
-    day1.part2()->printResult();
+    //Day1 day1;
+    //day1.part2()->printResult();
+
+    Day2 day2;
+    day2.part1();
 }
