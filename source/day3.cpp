c09e404436f57f8353c448b70f3f57e2d2a5f867
Chris <78105414+Chriz-code@users.noreply.github.com>
2025-12-03 17:12:27 +0100

    solved day3 part 1

--- source/day3.cpp
+++ source/day3.cpp
@@ -1,15 +1,29 @@
 #include "utils/filereader.cpp"
 #include <string>
+#include <sstream>
+#include <vector>
 
 class Day3
 {
 private:
-    int findIndexOfLargestNumberIn(string line) {
+    int charToInt(char c)
+    {
+        return c - '0';
+    }
+
+    int combine(int a, int b) {
+        return stoi(to_string(a) + to_string(b));
+    }
+
+    int findIndexOfLargestNumberIn(string line)
+    {
         int max = -1;
         int result = 0;
-        for (int i = 0; i < line.length(); i++) {
+        for (int i = 0; i < line.length(); i++)
+        {
             int value = line[i] - '0';
-            if (value > max) {
+            if (value > max)
+            {
                 max = value;
                 result = i;
             }
@@ -18,21 +32,61 @@ private:
     }
 
 public:
-// 17922 too high
+    // 17922 too high
+    // 17766
     Day3 *part1()
     {
         FileReader reader("inputs/day3.txt");
 
         string line;
         long long sum = 0;
-        while (getline(*reader.stream, line)) {
-            int index = findIndexOfLargestNumberIn(line);
-            char first = line[index];
-            line[index] = '0';
-            index = findIndexOfLargestNumberIn(line);
-            char second = line[index];
+        while (getline(*reader.stream, line))
+        {
+            int numbers[2] = {charToInt(line[0]), charToInt(line[1])};
+            for (int i = 2; i < line.length(); i++)
+            {
+                int num = charToInt(line[i]);
+                if (i + 1 < line.length() && num > numbers[0])
+                {
+                    numbers[0] = num;
+                    numbers[1] = 0;
+                }
+                else if (num > numbers[1])
+                {
+                    numbers[1] = num;
+                }
+            }
+            cout << numbers[0] << numbers[1] << endl;
+            sum += combine(numbers[0], numbers[1]);
+        }
+        cout << "Love " << sum << endl;
+        return this;
+    }
 
-            sum += stoll(string({first, second}));
+    Day3 *part1Test()
+    {
+        stringstream ss(
+            "987654321111111\n811111111111119\n234234234234278\n818181911112111");
+        string line;
+        long long sum = 0;
+        while (getline(ss, line))
+        {
+            int numbers[2] = {charToInt(line[0]), charToInt(line[1])};
+            for (int i = 2; i < line.length(); i++)
+            {
+                int num = charToInt(line[i]);
+                if (i + 1 < line.length() && num > numbers[0])
+                {
+                    numbers[0] = num;
+                    numbers[1] = 0;
+                }
+                else if (num > numbers[1])
+                {
+                    numbers[1] = num;
+                }
+            }
+            cout << numbers[0] << numbers[1] << endl;
+            sum += combine(numbers[0], numbers[1]);
         }
         cout << "Love " << sum << endl;
         return this;
