42237d2ca82efb2c49954aa051f0588a2101cf2d
Chris <78105414+Chriz-code@users.noreply.github.com>
2025-12-15 11:20:59 +0100

    setup day11 and day12

---
+++ src/day11.cpp
@@ -0,0 +1,23 @@
+#include "utils/filereader.cpp"
+
+namespace Day11 {
+    class Solution {
+    private:
+        char* TEST_INPUT =
+            "aaa: you hhh"
+            "you: bbb ccc"
+            "bbb: ddd eee"
+            "ccc: ddd eee fff"
+            "ddd: ggg"
+            "eee: out"
+            "fff: out"
+            "ggg: out"
+            "hhh: ccc fff iii"
+            "iii: out";
+
+    public:
+        Solution *part1Test(){
+            return this;
+        }
+    };
+}
